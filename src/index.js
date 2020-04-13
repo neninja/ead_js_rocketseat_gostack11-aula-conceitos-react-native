50b11dffbe414032d03fd6f22d76ac3d35698d97
Felipe Silva <felipe.dacs@gmail.com>
2020-04-12 21:56:34 -0300

    release: add aula 4

--- src/index.js
+++ src/index.js
@@ -16,6 +16,15 @@ export default function App(){
     })
   }, [])
 
+  async function handleAddProject(){
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Felipe'
+    })
+
+    setProjects([...projects, response.data])
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1"/>
@@ -27,6 +36,15 @@ export default function App(){
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+
       </SafeAreaView>
     </>
   )
@@ -42,5 +60,19 @@ const styles = StyleSheet.create({
   project: {
     color: "#FFF",
     fontSize: 30,
+  },
+
+  button: {
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 16
   }
 })
